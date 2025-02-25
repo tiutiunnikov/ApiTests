17dc18b414f194a45aa9359322db9c10a45d4f3e
Konstantin Tiutiunnikov <konstantin.tiutiunnikov@gmail.com>
2025-02-25 12:04:09 +0300

    Подготовил пулл реквест для Домашки 2

--- tests/test_get_resource_data.py
+++ tests/test_get_resource_data.py
@@ -1,26 +1,41 @@
 import httpx
 from jsonschema import validate
-
 from core.contracts import LIST_DATA_SCHEME
+import allure
 
 base_url = 'https://reqres.in/'
 list_resource = 'api/unknown'
 single_resource = 'api/unknown/2'
 not_found_resource = 'api/unknown/23'
 
+@allure.suite('Проверка запросов данных ресурсов')
+@allure.title('Проверяем получение списка ресурсов')
 def test_list_resource():
-    response = httpx.get(base_url + list_resource)
-    assert response.status_code == 200
+    with allure.step(f'Делаем запрос по адресу: {base_url + list_resource}'):
+        response = httpx.get(base_url + list_resource)
+
+    with allure.step('Проверяем код ответа'):
+        assert response.status_code == 200
 
     data_list_resources = response.json()['data']
     for item in data_list_resources:
-        validate(item, LIST_DATA_SCHEME)
+        with allure.step('Проверяем структуру ответа'):
+            validate(item, LIST_DATA_SCHEME)
 
+@allure.suite('Проверка запросов данных одного ресурса')
+@allure.title('Проверяем код ответа одного ресурса')
 def test_single_resource():
-    response = httpx.get(base_url + single_resource)
-    assert  response.status_code == 200
+    with allure.step(f'Делаем запрос по адресу: {base_url + single_resource} '):
+        response = httpx.get(base_url + single_resource)
+    with allure.step('Проверяем код ответа'):
+        assert  response.status_code == 200
 
+@allure.suite('Проверка запросов ненайденного ресурса')
+@allure.title('Проверяем код ответа ненайденного ресурса')
 def test_not_found_resource():
-    response = httpx.get(base_url + not_found_resource)
-    assert  response.status_code == 404
-    assert  response.json() == {} # проверка на пустой словарь.
+    with allure.step(f'Делаем запрос по адресу: {base_url + not_found_resource}'):
+        response = httpx.get(base_url + not_found_resource)
+    with allure.step('Проверяем код ответа ненайденного ресурса'):
+        assert  response.status_code == 404
+    with allure.step('Проверка на пустой словарь'):
+        assert  response.json() == {} # проверка на пустой словарь
--- tests/test_get_user_data.py
+++ tests/test_get_user_data.py
@@ -3,6 +3,8 @@ from jsonschema import validate
 
 from core.contracts import USER_DATA_SCHEME
 
+import allure
+
 base_url = 'https://reqres.in/'
 list_users = 'api/users?page=2'
 single_user = 'api/users/2'
@@ -10,16 +12,25 @@ not_found_user = 'api/users/23'
 email_ends = '@reqres.in'
 avatar_ends = '-image.jpg'
 
+@allure.suite('Проверка запросов данных пользователей')
+@allure.title('Проверка статуса запроса')
 def test_list_users():
-    response = httpx.get(base_url + list_users)
-    assert response.status_code == 200
+    with allure.step(f'Делаем запрос по адресу {base_url + list_users}'):
+        response = httpx.get(base_url + list_users)
+    with allure.step('Проверяем код ответа'):
+        assert response.status_code == 200
+
     data = response.json()['data']
-
     for item in data:
-        validate(item, USER_DATA_SCHEME)
-        assert item['email'].endswith(email_ends)
-        assert item['avatar'].endswith(str(item['id']) + avatar_ends)
+        with allure.step((f'Проверяем элемент из списка')):
+            validate(item, USER_DATA_SCHEME)
+        with allure.step('Проверяем окончание E-mail адреса'):
+            assert item['email'].endswith(email_ends)
+        with allure.step('Проверяем наличие id в ссылке на аватар'):
+            assert item['avatar'].endswith(str(item['id']) + avatar_ends)
 
+@allure.suite('Проверка запроса данных одного пользователя')
+@allure.title('Проверка статуса запроса одного пользователя')
 def test_single_users():
     response = httpx.get(base_url + single_user)
     assert  response.status_code == 200
